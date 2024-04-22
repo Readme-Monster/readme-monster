15dea6efe9f57fdf86c43c7ae5e26d5a9d86abe9
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-04-23 00:31:27 +0900

    Feature/setting 2.1.2 (#37)
    * 초기 셋팅

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import '@testing-library/jest-dom';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
