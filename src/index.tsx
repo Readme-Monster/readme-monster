8443b13c437ad4fb5c84131fead8d6aa1e883708
이재협 <zzok3312@naver.com>
2024-04-23 21:02:30 +0900

    feat: tailwind setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App";
+import "../src/styles/global.css";
 
 const rootElement = document.getElementById("root");
 
