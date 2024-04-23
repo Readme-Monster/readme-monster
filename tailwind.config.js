c56d4ab0055fd157ac78606d56f3b6305647904f
이재협 <zzok3312@naver.com>
2024-04-23 23:16:02 +0900

    feat: tailwind.config theme colors 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textPrimary: "#343A40",
+        textSecondary: "#868E96",
+        textBlue: "#4B88FF",
+      }
+    },
   },
   plugins: [],
 };
-
