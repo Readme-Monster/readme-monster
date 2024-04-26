cf1280cb1a26719ab6b0f0dec9dd5fe32b4ed616
이재협 <zzok3312@naver.com>
2024-04-26 23:33:33 +0900

    feat: tab 버튼 추가

--- src/pages/builder/ReadmeBuilder.tsx
+++ src/pages/builder/ReadmeBuilder.tsx
@@ -1,18 +1,24 @@
+import Header from "common/Header/Header";
 import React from "react";
 import EditorPreviewContainer from "../../components/Editor/EditorPreviewContainer";
 import SectionsContainer from "../../components/Editor/SectionsContainer";
 
 const ReadmeBuilder = () => {
   return (
-    <div className="w-full h-[calc(100%_-_64px)]">
-      <div className="max-w-full h-full p-[35px] flex flex-row gap-[30px]">
-        <div className="max-w-[400px] w-full h-full">
-          <SectionsContainer/>
+    <div className="w-full h-full flex flex-col">
+      <div className="h-[80px] flex-Center">
+        <Header />
+      </div>
+      <div className="w-full h-[calc(100%_-_64px)]">
+        <div className="max-w-full h-full p-[30px] pt-[20px] flex flex-row gap-[30px]">
+          <div className="max-w-[400px] w-full h-full">
+            <SectionsContainer />
+          </div>
+          <div className="max-w-full w-full h-full flex flex-row gap-[30px]">
+            <EditorPreviewContainer />
+          </div>
         </div>
-        <div className="max-w-full w-full h-full flex flex-row gap-[30px]">
-          <EditorPreviewContainer/>
-        </div>
-      </div> 
+      </div>
     </div>
   );
 };
