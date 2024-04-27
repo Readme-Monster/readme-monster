964991a4565908e39ee24bcbc1fdea179b771a4c
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-04-27 16:37:43 +0900

    fix: conflict 해결 (#60)

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,7 +3,7 @@ import { Routes } from "./Routes";
 
 function App() {
   return (
-    <div className="flex-Center h-[100vh]">
+    <div className="App">
       <Routes />
     </div>
   );
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return <img src="/images/notfound.svg" alt="404" />;
+  return (
+    <div className="w-full h-[100vh] flex justify-center items-center">
+      <img src="/images/notfound.svg" alt="404" />
+    </div>
+  );
 };
 
 export default NotFoundPage;
