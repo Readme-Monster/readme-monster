cacaf4d0b63feb312bdd755531f9cca8c2fc71fe
이재협 <zzok3312@naver.com>
2024-04-27 23:34:28 +0900

    fix: edit page ui 수정 및 gnb 높이, 홈페이지 css 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,21 +12,21 @@ interface ButtonProps {
 
 function HomePage() {
   return (
-    <div className="w-full h-[calc(100vh-48px)] flex flex-col items-center justify-center">
+    <div className="w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-[20px]">
       <div className="flex-Center flex-col" data-testid="home">
-        <img alt="로고" src="/img/logo.png" width="140" height="140" data-testid="logo" />
+        <img alt="로고" src="/img/logo.png" width="150" height="150" data-testid="logo" />
         <span className="text-textPrimary  text-3xl md:text-6xl sm:text-5xl font-bold line-lg" data-testid="title">
           README-MONSTER
         </span>
         <p
-          className="max-w-md mx-auto mt-3 text-textSecondary text-gray-500 display-contents sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+          className="max-w-md mx-auto mt-3 text-textSecondary display-contents sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
           data-testid="description"
         >
           <span>당신의 프로젝트 README를</span>
           <span>자동으로 만들 수 있는 가장 빠르고 쉬운 방법</span>
         </p>
       </div>
-      <div className="flex justify-center mt-6" data-testid="button">
+      <div className="flex flex-row gap-[20px]" data-testid="button">
         <Button
           url="/editor"
           color="text-white"
@@ -59,7 +59,7 @@ export function Button({ url, color, backgroundColor, description, hover, testId
   return (
     <div className="flex flex-col items-center">
       <span className="inline-flex rounded-md shadow">
-        <a
+        <div
           className={`
           inline-flex items-center px-4 py-2 
           font-medium text-xl 
@@ -68,12 +68,14 @@ export function Button({ url, color, backgroundColor, description, hover, testId
           border border-transparent rounded-lg 
           ${color} 
           w-[250px] h-[54px] 
-          justify-center`}
+          justify-center
+          cursor-pointer
+          `}
           onClick={handleClick}
           data-testid={testId}
         >
           {description}
-        </a>
+        </div>
       </span>
     </div>
   );
