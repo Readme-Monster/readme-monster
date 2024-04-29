113ae4e4fc14d2fd6febc3a3820e12d38e804200
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-04-30 04:30:20 +0900

    Test: "로그인 버튼 클릭 시 이벤트 발생 및 로직 실행 확인"

--- __tests__/unit/LoginPage/Page.spec.tsx
+++ __tests__/unit/LoginPage/Page.spec.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 import React from "react";
@@ -15,5 +15,36 @@ describe("LoginPage 테스트", () => {
     );
 
     expect(screen.getByTestId("login")).toBeInTheDocument();
+  });
+  test("로그인 버튼 클릭 시 제출 이벤트 발생", async () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <LoginPage />
+      </MemoryRouter>,
+    );
+
+    const usernameInput = screen.getByPlaceholderText("아이디를 입력해주세요");
+    const passwordInput = screen.getByPlaceholderText("비밀번호를 입력해주세요");
+    const loginButton = await screen.findByTestId("login-button");
+
+    await act(async () => {
+      fireEvent.change(usernameInput, { target: { value: "testuser" } });
+      fireEvent.change(passwordInput, { target: { value: "testpassword" } });
+    });
+
+    console.log("Current State:", {
+      username: usernameInput.value,
+      password: passwordInput.value,
+    });
+
+    const consoleSpy = jest.spyOn(console, "log");
+
+    await act(async () => {
+      fireEvent.click(loginButton);
+    });
+
+    expect(consoleSpy).toHaveBeenCalledWith("Form Submitted:", { username: "testuser", password: "testpassword" });
+
+    consoleSpy.mockRestore();
   });
 });
