6dce033111271f0ac93b2945bb695a7678853a41
이재협 <zzok3312@naver.com>
2024-04-29 22:19:49 +0900

    feat: drag & drop 기능 구현

--- src/components/Editor/Components/EditSection.tsx
+++ src/components/Editor/Components/EditSection.tsx
@@ -1,32 +1,57 @@
 import React, { useState } from "react";
 import { List, Reset, TrashCan } from "@carbon/icons-react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import clsx from "clsx";
 
-const EditSection = () => {
-  const [hover, setHover] = useState(false);
+interface Props {
+  id: number;
+  title: string | undefined;
+}
+
+const EditSection = ({ id, title }: Props) => {
+  const [hover, setHover] = useState<boolean>(false);
+  const [clicked, setClicked] = useState<boolean>(false);
+
+  const onClickDiv = () => setClicked(!clicked);
 
   const onMouseEnter = () => setHover(true);
   const onMouseLeave = () => setHover(false);
 
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
   return (
     <div
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
+      onClick={() => onClickDiv()}
       onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave} 
-      className="
-      w-full h-[45px] py-[8px] px-[12px]
-      flex flex-row gap-[10px] items-center
-      rounded-[8px] border-solid border bg-white border-[#F1F3F5] drop-shadow-[0_1px_1px_rgba(173,181,189,0.25)] 
-      cursor-pointer"
+      onMouseLeave={onMouseLeave}
+      className={clsx(
+        "w-full h-[45px] py-[8px] px-[12px]",
+        "flex flex-row gap-[10px] items-center",
+        "rounded-[8px] border-solid border bg-white border-[#F1F3F5] drop-shadow-[0_1px_1px_rgba(173,181,189,0.25)]",
+        "cursor-pointer",
+        {
+          "focus:outline-none focus:ring-2 focus:ring-textBlue": !clicked,
+        },
+      )}
     >
-      <List size={25} className="fill-textSecondary"/>
-      <div>
-        <p className="text-textPrimary">
-        </p>
-      </div>
-      {hover && 
+      <List size={25} className="fill-textSecondary min-w-[25px]" />
+      <p className="text-textPrimary mb-0 truncate">{title}</p>
+      {hover && (
         <div className="flex flex-row gap-[10px] ml-auto">
-          <Reset size={20} className="fill-[#ADB5BD]" onClick={() => alert("rest")}/>
-          <TrashCan size={20} className="fill-textPrimary" onClick={() => alert("delete")}/>
-        </div>}
+          <Reset size={20} className="fill-[#ADB5BD]" onClick={() => alert("rest")} />
+          <TrashCan size={20} className="fill-textPrimary" onClick={() => alert("delete")} />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Editor/Components/EditSections.tsx
+++ src/components/Editor/Components/EditSections.tsx
@@ -1,16 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EditSection from "./EditSection";
+import {
+  DndContext,
+  closestCenter,
+  useSensor,
+  useSensors,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+  arrayMove,
+  sortableKeyboardCoordinates,
+} from "@dnd-kit/sortable";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
 
 const EditSections = () => {
+  const mockList: { id: number; title: string | undefined }[] = [
+    {
+      id: 1,
+      title: "drag & drop Test 1",
+    },
+    {
+      id: 2,
+      title: "drag & drop Test 2",
+    },
+    {
+      id: 3,
+      title: "drag & drop Test 3",
+    },
+  ];
+
+  const [sections, setSections] = useState(mockList);
+
+  const getIndex = (id: number) => sections.findIndex(el => el.id === id);
+
+  const handleDragEnd = (event: any) => {
+    const { active, over } = event;
+    if (active.id === over.id) return;
+    setSections(sections => {
+      const oldIndex = getIndex(active.id);
+      const newIndex = getIndex(over.id);
+
+      return arrayMove(sections, oldIndex, newIndex);
+    });
+  };
+
+  const sensors = useSensors(
+    useSensor(TouchSensor),
+    useSensor(MouseSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  useEffect(() => {
+    localStorage.setItem("sections-list", JSON.stringify(sections));
+  }, [sections]);
+
   return (
     <div className="flex flex-col gap-[10px] px-[10px]">
       <div className="flex-Center flex-row justify-between min-h-[30px]">
         <p className="text-textPrimary ml-[5px] mb-0 text-sm">Edit Section</p>
       </div>
-      <div className="flex flex-col gap-[10px]">
-        <EditSection />
-        <EditSection />
-        <EditSection />
+      <div className="flex flex-col gap-[10px] h-full">
+        <DndContext
+          sensors={sensors}
+          onDragEnd={handleDragEnd}
+          collisionDetection={closestCenter}
+          modifiers={[restrictToVerticalAxis]}
+        >
+          <SortableContext items={sections} strategy={verticalListSortingStrategy}>
+            {sections.map(section => (
+              <EditSection key={section.id} title={section.title} id={section.id} />
+            ))}
+          </SortableContext>
+        </DndContext>
       </div>
     </div>
   );
