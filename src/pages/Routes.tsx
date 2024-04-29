951357e7c8509cf5b55fa82ae08990c6e4b3144a
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-04-29 13:27:32 +0900

    Feat: "로그인,회원가입 페이지 생성"

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -5,12 +5,16 @@ import HomePage from "./HomePage";
 import NotFoundPage from "./NotFoundPage/NotFoundPage";
 import ReadmeBuilder from "./builder/ReadmeBuilder";
 import MyPage from "./myPage";
+import LoginPage from "./LoginPage";
+import SignupPage from "./SignupPage";
 
 export const Routes = () => {
   return (
     <ReactRouterRoutes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="editor" element={<ReadmeBuilder />} />
         <Route path="myPage" element={<MyPage />} />
       </Route>
