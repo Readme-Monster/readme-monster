483ff8b7576d94e5ebfab2ad1e5340df1e7763d4
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-04-29 13:34:57 +0900

    Fix: "Common 컴포넌트 병합"

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../common/Header/Header";
+import Header from "../components/Common/Header/Header";
 
 const AppLayout = () => {
   return (
