20c4ed594146432555477784335f0d61d2059ae5
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-05-01 11:00:13 +0900

    Feat: "firebase 세팅"

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./pages/App";
+import firebase from "./hooks/firebaseApp";
 import "../src/styles/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { SectionProvider } from "context/SectionContext";
 
 const rootElement = document.getElementById("root");
+
+console.log(firebase);
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
