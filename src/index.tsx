ddfe85d22f9e2d8417bd1ca952578db57aaaa4ec
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-05-01 16:43:52 +0900

    feat: "firebase auth 세팅"

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./pages/App";
-import firebase from "./hooks/firebaseApp";
+import firebase from "./firebaseApp";
 import "../src/styles/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { SectionProvider } from "context/SectionContext";
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,17 @@
+import { getAuth } from "firebase/auth";
+import { useEffect, useState } from "react";
 import React from "react";
 import { Routes } from "./Routes";
+import { app } from "../firebaseApp";
+import LoadingSpinner from "../components/Common/LoadingSpinner/LoadingSpinner";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes />
-    </div>
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
   );
+
+  return <Routes isAuthenticated={isAuthenticated} />;
 }
 
 export default App;
