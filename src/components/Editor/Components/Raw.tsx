c5346fa29436fd04d5f25f8f53722fda56d9b414
이재협 <zzok3312@naver.com>
2024-05-02 18:48:05 +0900

    feat: 마크다운  복사하기 기능 구현

--- src/components/Editor/Components/Raw.tsx
+++ src/components/Editor/Components/Raw.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Copy } from "@carbon/icons-react";
 import { ValueType } from "../types";
 
 const Raw = ({ value }: ValueType) => {
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const handleCopyClick = async (textToCopy: string) => {
+    textAreaRef.current?.select();
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      alert("Copying is complete.");
+    } catch (e) {
+      alert("Copying failed.");
+    }
+  };
+
   return (
     <div className="w-full h-full p-[20px] rounded-[8px] border-solid border border-textTertiary relative">
-      <div className="h-auto absolute right-[20px]">
-        <Copy size={18} className="cursor-pointer" onClick={() => alert("복사하기가 완료되었습니다.")} />
-      </div>
-      <textarea key={value} readOnly className="h-full w-full resize-none focus:outline-none p-0">
+      {value!.length > 0 && (
+        <div className="h-auto absolute right-[20px]" onClick={() => handleCopyClick(value!)}>
+          <Copy size={18} className="cursor-pointer" />
+        </div>
+      )}
+      <textarea ref={textAreaRef} key={value} readOnly className="h-full w-full resize-none focus:outline-none p-0">
         {value}
       </textarea>
     </div>
