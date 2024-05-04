a21e04e4e9bae3d2dcf9b32180468629af85f3d6
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-05-04 15:30:03 +0900

    Test: "로그인 성능 테스트"

--- __tests__/unit/LoginPage/Page.spec.tsx
+++ __tests__/unit/LoginPage/Page.spec.tsx
@@ -1,12 +1,26 @@
-import { fireEvent, render, screen, act } from "@testing-library/react";
+import { fireEvent, render, screen, act, waitFor } from "@testing-library/react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
-import React from "react";
+import { toast } from "react-toastify";
 import "@testing-library/jest-dom";
+import React from "react";
 
 import LoginPage from "../../../src/pages/LoginPage";
 
+jest.mock("firebase/auth");
+jest.mock("react-toastify", () => ({
+  toast: {
+    error: jest.fn(),
+    success: jest.fn(),
+  },
+}));
+
 describe("LoginPage 테스트", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   test("LoginPage 정상적으로 렌더링되어야 함", () => {
     render(
       <MemoryRouter initialEntries={["/"]}>
@@ -16,6 +30,37 @@ describe("LoginPage 테스트", () => {
 
     expect(screen.getByTestId("login")).toBeInTheDocument();
   });
+
+  test("유효성검사 : 이메일", async () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <LoginPage />
+      </MemoryRouter>,
+    );
+
+    const emailInput = screen.getByPlaceholderText("이메일을 입력해주세요");
+    await act(async () => {
+      fireEvent.change(emailInput, { target: { value: "invalidemail" } });
+    });
+
+    expect(screen.getByText("이메일 형식이 올바르지 않습니다.")).toBeInTheDocument();
+  });
+
+  test("유효성검사 :비밀번호", async () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <LoginPage />
+      </MemoryRouter>,
+    );
+
+    const passwordInput = screen.getByPlaceholderText("비밀번호를 입력해주세요");
+    await act(async () => {
+      fireEvent.change(passwordInput, { target: { value: "short" } });
+    });
+
+    expect(screen.getByText("비밀번호는 8자리 이상으로 입력해주세요")).toBeInTheDocument();
+  });
+
   test("로그인 버튼 클릭 시 제출 이벤트 발생", async () => {
     render(
       <MemoryRouter initialEntries={["/"]}>
@@ -23,25 +68,40 @@ describe("LoginPage 테스트", () => {
       </MemoryRouter>,
     );
 
-    const userIdInput = screen.getByPlaceholderText("아이디를 입력해주세요");
+    const emailInput = screen.getByPlaceholderText("이메일을 입력해주세요");
     const passwordInput = screen.getByPlaceholderText("비밀번호를 입력해주세요");
     const loginButton = await screen.findByTestId("login-button");
 
     await act(async () => {
-      fireEvent.change(userIdInput, { target: { value: "testuser" } });
+      fireEvent.change(emailInput, { target: { value: "test@example.com" } });
       fireEvent.change(passwordInput, { target: { value: "testpassword" } });
-    });
-
-    const consoleSpy = jest.spyOn(console, "log");
-
-    await act(async () => {
       fireEvent.click(loginButton);
     });
 
-    expect(consoleSpy).toHaveBeenCalledWith("Form Submitted:", { id: "testuser", password: "testpassword" });
-
-    consoleSpy.mockRestore();
+    expect(toast.success).toHaveBeenCalledWith("로그인에 성공했습니다.");
   });
+
+  test("잘못된 로그인 정보 제출 시 토스트 에러 메시지", async () => {
+    signInWithEmailAndPassword.mockImplementation(() => Promise.reject({ code: "auth/wrong-password" }));
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <LoginPage />
+      </MemoryRouter>,
+    );
+
+    const emailInput = screen.getByPlaceholderText("이메일을 입력해주세요");
+    const passwordInput = screen.getByPlaceholderText("비밀번호를 입력해주세요");
+    const loginButton = screen.getByTestId("login-button");
+
+    await act(async () => {
+      fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+      fireEvent.change(passwordInput, { target: { value: "wrongpassword" } });
+      fireEvent.click(loginButton);
+    });
+
+    expect(toast.error).toHaveBeenCalledWith("잘못된 이메일이나 비밀번호입니다.");
+  });
+
   test("url클릭 시 '/signup'로 이동한다", () => {
     const pushMock = jest.fn();
 
