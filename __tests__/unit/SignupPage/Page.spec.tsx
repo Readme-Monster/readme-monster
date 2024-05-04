aba1f5735a06a82bfdc60d539b37a83fd7dba910
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-05-04 15:29:43 +0900

    Test:"회원가입 기능 테스트"

--- __tests__/unit/SignupPage/Page.spec.tsx
+++ __tests__/unit/SignupPage/Page.spec.tsx
@@ -1,53 +1,88 @@
-import { fireEvent, render, screen, act } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { fireEvent, render, screen, act, waitFor } from "@testing-library/react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { MemoryRouter } from "react-router-dom";
+import { toast } from "react-toastify";
 import React from "react";
 import "@testing-library/jest-dom";
 
 import SignupPage from "../../../src/pages/SignupPage";
 
-describe("LoginPage 테스트", () => {
-  test("LoginPage 정상적으로 렌더링되어야 함", () => {
+jest.mock("firebase/auth", () => ({
+  getAuth: jest.fn(),
+  createUserWithEmailAndPassword: jest.fn()
+}));
+
+jest.mock("react-toastify", () => ({
+  toast: {
+    error: jest.fn(),
+    success: jest.fn(),
+  },
+}));
+
+describe("SignupPage 테스트", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    createUserWithEmailAndPassword.mockResolvedValue({ user: { email: "test@example.com", uid: "123456" }});
+  });
+
+  test("SignupPage 정상적으로 렌더링되어야 함", () => {
     render(
-      <MemoryRouter initialEntries={["/"]}>
+      <MemoryRouter initialEntries={["/signup"]}>
         <SignupPage />
       </MemoryRouter>,
     );
 
     expect(screen.getByTestId("signup")).toBeInTheDocument();
   });
-  test("회원가입 버튼 클릭 시 제출 이벤트 발생", async () => {
+
+  test("유효한 정보 입력 시 회원가입 성공", async () => {
     render(
-      <MemoryRouter initialEntries={["/"]}>
+      <MemoryRouter initialEntries={["/signup"]}>
         <SignupPage />
       </MemoryRouter>,
     );
 
-    const userIdInput = screen.getByPlaceholderText("아이디를 입력해주세요");
-    const userNameInput = screen.getByPlaceholderText("이름을 입력해주세요");
-    const phoneNumberInput = screen.getByPlaceholderText("전화번호를 입력해주세요");
+    const emailInput = screen.getByPlaceholderText("이메일을 입력해주세요");
+    const nameInput = screen.getByPlaceholderText("이름을 입력해주세요");
     const passwordInput = screen.getByPlaceholderText("비밀번호를 입력해주세요");
     const passwordCheckInput = screen.getByPlaceholderText("비밀번호를 다시 입력해주세요");
-    const signpButton = await screen.findByTestId("signup-button");
+    const signupButton = screen.getByTestId("signup-button");
 
     await act(async () => {
-      fireEvent.change(userIdInput, { target: { value: "testuser" } });
-      fireEvent.change(userNameInput, { target: { value: "testusername" } });
-      fireEvent.change(phoneNumberInput, { target: { value: "testphonenumber" } });
-      fireEvent.change(passwordInput, { target: { value: "testpassword" } });
-      fireEvent.change(passwordCheckInput, { target: { value: "testpassword" } });
+      fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+      fireEvent.change(nameInput, { target: { value: "Test Name" } });
+      fireEvent.change(passwordInput, { target: { value: "12345678" } });
+      fireEvent.change(passwordCheckInput, { target: { value: "12345678" } });
     });
-
     const consoleSpy = jest.spyOn(console, "log");
-
     await act(async () => {
-      fireEvent.click(signpButton);
+      fireEvent.click(signupButton);
     });
 
-    expect(consoleSpy).toHaveBeenCalledWith("Form Submitted:", { id: "testuser", name: "testusername", phone: "testphonenumber", password: "testpassword", passwordCheck: "testpassword", });
-
-    consoleSpy.mockRestore();
+    expect(consoleSpy).toHaveBeenCalledWith("Form Submitted:", { email: "test@example.com", name: "Test Name", password: "12345678", passwordCheck: "12345678", });
+    expect(toast.success).toHaveBeenCalledWith("회원가입에 성공했습니다.");
   });
+
+  test("유효성검사 : 비밀번호 재확인", async () => {
+    render(
+      <MemoryRouter>
+        <SignupPage />
+      </MemoryRouter>,
+    );
+
+    const passwordInput = screen.getByPlaceholderText("비밀번호를 입력해주세요");
+    const passwordCheckInput = screen.getByPlaceholderText("비밀번호를 다시 입력해주세요");
+
+    fireEvent.change(passwordInput, { target: { value: "12345678" } });
+    fireEvent.change(passwordCheckInput, { target: { value: "123456789" } });
+
+    fireEvent.blur(passwordCheckInput);
+
+    await waitFor(() =>
+      expect(screen.getByText("비밀번호와 비밀번호 확인 값이 다릅니다. 다시 확인해주세요.")).toBeInTheDocument(),
+    );
+  });
+
   test("url클릭 시 '/login'로 이동한다", () => {
     const pushMock = jest.fn();
 
@@ -55,7 +90,7 @@ describe("LoginPage 테스트", () => {
       push: pushMock,
     });
     render(
-      <MemoryRouter initialEntries={["/"]}>
+      <MemoryRouter>
         <SignupPage />
       </MemoryRouter>,
     );
