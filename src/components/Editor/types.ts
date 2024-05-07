4c061bdbe41a146a7aa26034e6739ae8aa6739ed
이재협 <zzok3312@naver.com>
2024-05-07 13:18:52 +0900

    fix: EditSection 주석 처리

--- src/components/Editor/types.ts
+++ src/components/Editor/types.ts
@@ -6,7 +6,6 @@ export interface SectionsType {
   id: number;
   title: string | undefined;
   markdown: string | undefined;
-  onDeleteSection: (e: React.MouseEvent<HTMLElement, MouseEvent>, targetId: number) => void;
 }
 
 export interface KeyNameType {
