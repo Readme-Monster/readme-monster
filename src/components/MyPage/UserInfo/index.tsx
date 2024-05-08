3646010b7937bddec806c6e6ff7f7c18860a9d45
최규성 <kyusung612@gmail.com>
2024-05-09 00:19:20 +0900

    [FEAT] 마이페이지 firebase 연동 및 회원 탈퇴 기능 구현

--- src/components/MyPage/UserInfo/index.tsx
+++ src/components/MyPage/UserInfo/index.tsx
@@ -1,30 +1,42 @@
-import React, { ChangeEvent, useState, useContext } from "react";
-import AuthContext from "context/AuthContext";
+import React, { ChangeEvent, useState, useEffect } from "react";
+
 import Input from "../Input";
 import { UserInfoProps } from "./types";
-import { getAuth, deleteUser } from "firebase/auth";
-import { app } from "firebaseApp";
+import { getAuth, signOut } from "firebase/auth";
 import { toast } from "react-toastify";
 import { useRouter } from "pages/routing";
+import { app, db } from "../../../firebaseApp";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 
 const UserInfo = () => {
   const [userInfo, setUserInfo] = useState<UserInfoProps>({
     name: "",
-    id: "",
     email: "",
     password: "",
     passwordCheck: "",
+    docId: "",
   });
 
-  const { user } = useContext(AuthContext);
-
-  console.log(user);
-  
   const auth = getAuth(app);
   const router = useRouter();
 
-  console.log(auth.currentUser?.uid);
-  console.log("=============");
+  const handleGetUserInfo = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "userInfo"));
+
+      querySnapshot.forEach(doc => {
+        const { name, email } = doc.data();
+        setUserInfo(prev => ({
+          ...prev,
+          name,
+          email,
+          docId: doc.id,
+        }));
+      });
+    } catch (e: any) {
+      toast.error("오류가 발생했습니다");
+    }
+  };
 
   const handleUpdateUserInfo = (e: ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
@@ -37,22 +49,26 @@ const UserInfo = () => {
 
   const handleDeleteUser = async () => {
     try {
-      await deleteUser(auth.currentUser!).then(res => {
-        console.log(res);
-      });
+      await deleteDoc(doc(db, "userInfo", userInfo.docId));
+      await signOut(auth);
 
       toast.success("회원탈퇴를 완료하였습니다.");
+
       router.push("/");
     } catch (error: any) {
       console.log(error);
     }
   };
 
+  useEffect(() => {
+    handleGetUserInfo();
+  }, []);
+
   return (
     <div className="w-1/2 h-full flex-Center flex-col gap-2 border-1 p-2 border-cyan-600">
       <img className="w-28 h-28 bg-slate-400 rounded-full" src="" />
-      <Input value={userInfo.name} placeholder="이름" id="name" onChange={handleUpdateUserInfo} />
-      <Input value={userInfo.email} placeholder="이메일" id="email" onChange={handleUpdateUserInfo} />
+      <Input value={userInfo.name} placeholder="이름" id="name" onChange={handleUpdateUserInfo} disabled={true} />
+      <Input value={userInfo.email} placeholder="이메일" id="email" onChange={handleUpdateUserInfo} disabled={true} />
       <Input
         value={userInfo.password}
         placeholder="패스워드"
