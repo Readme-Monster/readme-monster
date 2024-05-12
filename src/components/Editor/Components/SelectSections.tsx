4e67d81c832114d93c04eba3100881f1965b2718
이재협 <zzok3312@naver.com>
2024-05-12 17:47:35 +0900

    fix: QA editor page css 수정

--- src/components/Editor/Components/SelectSections.tsx
+++ src/components/Editor/Components/SelectSections.tsx
@@ -66,7 +66,7 @@ const SelectSections = () => {
   return (
     <div className="h-full flex flex-col gap-[10px] px-[10px]">
       <div className="flex-Center flex-row justify-between min-h-[40px]">
-        <p className="text-textPrimary ml-[5px] mb-0 text-sm">Select Section</p>
+        <p className="text-textPrimary ml-[5px] mb-0 text-sm dark:text-textWhite">Select Section</p>
         <Add size={30} className="fill-textBlue cursor-pointer" onClick={openModalAlert} />
       </div>
       <div className="h-full max-h-auto flex flex-col gap-[10px]">
