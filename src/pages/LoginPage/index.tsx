10abb67377053040fbc85967ba66279a50502743
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-05-13 12:44:21 +0900

    Feat: "구글로 로그인 성공 시 경로 수정"

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -88,8 +88,8 @@ const LoginPage = () => {
     const result = await signInWithPopup(auth, provider);
     try {
       console.log(result.user);
-      toast.success("회원가입에 성공했습니다.");
-      router.push("/login");
+      toast.success("로그인에 성공했습니다.");
+      router.push("/");
       return result.user;
     } catch (error : any) {
       console.error("Error signing in with Google:", error);
