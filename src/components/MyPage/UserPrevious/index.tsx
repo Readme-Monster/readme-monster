f9817c51de1f077991a5726b84eff6a7ac8bca8e
최규성 <kyusung612@gmail.com>
2024-05-16 23:43:25 +0900

    [FIX-27] 마이페이지 이미지 오류 및 리스트 삭제 기능 구현

--- src/components/MyPage/UserPrevious/index.tsx
+++ src/components/MyPage/UserPrevious/index.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { UserSectionList } from "../UserInfo/types";
+import { UserInfoProps, UserSectionList, NewSectionList } from "../UserInfo/types";
+import { TrashCan } from "@carbon/icons-react";
+import { updateDoc, doc, getDoc } from "firebase/firestore";
+import { db } from "../../../firebaseApp";
+import { toast } from "react-toastify";
 
-const UserPreviousList = ({ userSectionList }: { userSectionList: UserSectionList[] }) => {
+const UserPreviousList = ({
+  userInfo,
+  userSectionList,
+  handleGetUserInfo,
+}: {
+  userInfo: UserInfoProps;
+  userSectionList: UserSectionList[];
+  handleGetUserInfo: () => void;
+}) => {
+  const [newUserSectionList, setNewUserSectionList] = useState<NewSectionList[]>();
   const navigate = useNavigate();
   const handleConvertDate = (timeStamp: number) => {
     const milliseconds = timeStamp * 1000;
@@ -33,38 +46,65 @@ const UserPreviousList = ({ userSectionList }: { userSectionList: UserSectionLis
       return "More than 30 Days Ago";
     }
   };
-  userSectionList.sort((a, b) => b.saveDate.seconds - a.saveDate.seconds);
-
-  userSectionList.forEach(user => {
-    user.dateType = handleCategorizeDate(user.saveDate.seconds);
-  });
-
-  const groupedData: { [key: string]: UserSectionList[] } = {};
-  userSectionList.forEach(user => {
-    const dateType = user.dateType;
-    if (dateType) {
-      groupedData[dateType] = groupedData[dateType] || [];
-      groupedData[dateType].push(user);
-    }
-  });
-
-  const newUserSectionList = Object.entries(groupedData).map(([dateType, data]) => ({ dateType, data }));
 
   const handleOnClick = (id: number) => {
     navigate(`/editor?id=${id}`);
   };
+
+  const handleDeleteClick = async (event: React.MouseEvent<HTMLDivElement, MouseEvent>, id: number) => {
+    event.stopPropagation();
+
+    try {
+      const userInfoRef = doc(db, "userInfo", userInfo.docId);
+      const userInfoSnap = await getDoc(userInfoRef);
+      const userInfoData = userInfoSnap.data();
+      const index = userInfoData?.sections.findIndex((section: any) => section.id === id);
+
+      if (index !== -1 && userInfoData) {
+        const updatedSections = [...userInfoData.sections.slice(0, index), ...userInfoData.sections.slice(index + 1)];
+        await updateDoc(userInfoRef, { sections: updatedSections });
+      }
+
+      handleGetUserInfo();
+    } catch (error) {
+      toast.success("오류가 발생했습니다.");
+    }
+  };
+
+  useEffect(() => {
+    if (userSectionList) {
+      userSectionList.sort((a, b) => b.saveDate.seconds - a.saveDate.seconds);
+
+      userSectionList.forEach(user => {
+        user.dateType = handleCategorizeDate(user.saveDate.seconds);
+      });
+
+      const groupedData: { [key: string]: UserSectionList[] } = {};
+      userSectionList.forEach(user => {
+        const dateType = user.dateType;
+        if (dateType) {
+          groupedData[dateType] = groupedData[dateType] || [];
+          groupedData[dateType].push(user);
+        }
+      });
+      const newList = Object.entries(groupedData).map(([dateType, data]) => ({ dateType, data }));
+
+      setNewUserSectionList(newList);
+    }
+  }, [userSectionList]);
+
   return (
     <div className="w-full h-full flex flex-col gap-2 pt-3 ">
       {newUserSectionList?.map((userData, i) => {
         const { dateType, data } = userData;
         return (
-          <div key={i} className="w-full flex flex-col gap-2">
+          <div key={i} className="w-full flex flex-col gap-2 relative">
             <div className="left-0 px-2 py-1 text-xs font-semibold dark:text-slate-50">{dateType}</div>
             {data.map(list => {
               return (
                 <div
                   key={list.id}
-                  className="relative w-full h-20 flex justify-center items-center bg-gray-800 dark:bg-slate-300 hover:scale-95 overflow-visible transition-transform ease-in-out duration-500 cursor-pointer"
+                  className="relative w-full h-20 flex justify-center items-center bg-gray-800 dark:bg-slate-300 hover:scale-95 overflow-visible transition-transform ease-in-out duration-500 cursor-pointer z-0"
                   onClick={() => handleOnClick(list.id)}
                 >
                   <h1
@@ -73,7 +113,13 @@ const UserPreviousList = ({ userSectionList }: { userSectionList: UserSectionLis
                   >
                     {list.editSections[0].title}
                   </h1>
-                  <div className="absolute top-1 right-1 px-2 py-1 text-xs text-white bg-gray-600 rounded-full">
+                  <div
+                    className="absolute top-2 right-1 px-2 py-1 text-xs text-white bg-gray-600 rounded-full z-10"
+                    onClick={event => handleDeleteClick(event, list.id)}
+                  >
+                    <TrashCan size={22} color="red" />
+                  </div>
+                  <div className="absolute top-2 left-1 px-2 py-1 text-xs text-white bg-gray-600 rounded-full z-40">
                     {handleConvertDate(list.saveDate?.seconds)}
                   </div>
                 </div>
